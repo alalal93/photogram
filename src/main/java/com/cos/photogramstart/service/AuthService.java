dfcb4f0e156fc743ff083c8e3d908ee4872d01e4
alalal93 <alalal9310@gmail.com>
2024-02-25 14:28:13 +0900

    회원가입 및 비밀번호 암호화

--- src/main/java/com/cos/photogramstart/service/AuthService.java
+++ src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -1,19 +1,30 @@
 package com.cos.photogramstart.service;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service// 1.Ioc 2.트랜잭션관리
 public class AuthService {
 	
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	
-	public void User 회원가입(User user) {
+	@Transactional //Write(Insert, Update, Delete)
+	public  User 회원가입(User user) { // 회원가입 메서드에 매개변수로 존재하는 User 오브젝트는 외부에서 클라이언트가 통신을 통해서 전송받은데이터
 		// 회원가입 진행
-		User userEntity = userRepository.save(user);
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		user.setRole("ROLE_USER"); //관리자 ROLE_ADMIN
+		User userEntity = userRepository.save(user); // userRepository.save()통해서 받은 User 오브젝트는 DB에 있는 데이터를 응답받은것
+		// 내가받은 데이터가 User 오브젝트 타입이므로 타입도 User return도 DB에서 받은 User 오브젝트인 userIntity를 해준다.
 		return userEntity;
 	}
 }
--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +13,24 @@ import com.cos.photogramstart.web.dto.auth.SignupDto;
 
 import lombok.RequiredArgsConstructor;
 	
-@RequiredArgsConstructor //final필드 DI할때 사용
+
+
+@RequiredArgsConstructor //final필드 DI할때 사용 final걸린 애들을 골라서 생성자를 자동으로 만들어준다
 @Controller
 public class AuthController {
 	
 	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 	
+	@Autowired // 첫번째방법 AutoController 가 AuthService를 의존하는관계 따라서 의존성 주입(DI를받아야한다)
+						//Controller 어노테이션이 붙어있으면 springboot가 authcontroller를 IOC컨테이너 관리하고있는 메모리에다가
+						//객체를 생성해서 Load해주는데 객체생성시 조건이필요하다 생성자를 만들어주는것 생성자를 실행시킬땐 Ioc컨테이너 등록되잇는
+						//데이터를 찾아 매개변수로 넣게됨 그걸찾아서 생성자에게 주입한후 컨트롤러가 Ioc컨테이너에 등록이되는것
+	
+	
+	// 세번째방법 의존성을 주입받을 AuthService에 final을 걸고 컨트롤러에 어노테이션 걸어주는방법
 	private final AuthService authService;
 	
+	// 두번째방법 AuthController의 생성자를 만들어서 의존성을 주입받음
 //	public AuthController(AuthService authService) {
 //		this.authService = authService;
 //	}
@@ -34,16 +45,18 @@ public class AuthController {
 			return "auth/signup";		
 	}
 	
-	//회원가입--> /auth/signup -> /ayth/signin
+	//회원가입--> /auth/signup -> /auth/signin
 	// 회원가입버튼X
 	@PostMapping("/auth/signup")
 	public String signup(SignupDto signupDto) {//key=value(x-www-form-urlencoded)
 		log.info(signupDto.toString());
 		// User <-- Signup Dto
+		// User object에 Signup Dto 데이터를 삽입하려고 한다.
+		
+		// User 오브젝트에 signDto에서 방금 만들었던 toEntity 데이터를 넣어주자
 		User user = signupDto.toEntity();
 		log.info(user.toString());
 		User userEntity = authService.회원가입(user);
-		
 		System.out.println(userEntity);
 		return "auth/signin";
 	}
