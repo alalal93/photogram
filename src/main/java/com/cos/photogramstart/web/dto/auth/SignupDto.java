665705bf213d5197bf208d93d1bde27dbd1c5732
alalal93 <alalal9310@gmail.com>
2024-02-25 16:51:50 +0900

    유효성검사

--- src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/auth/SignupDto.java
@@ -4,15 +4,24 @@ package com.cos.photogramstart.web.dto.auth;
 
 
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import com.cos.photogramstart.domain.user.User;
 
 import lombok.Data;
 
 @Data //Getter, Setter
 public class SignupDto {
+	//https://bamdule.tistory.com/35 (@valid 어노테이션종류)
+	@Size(min=2, max=20)
+	@NotBlank
 	private String username;
+	@NotBlank
 	private String password;
+	@NotBlank
 	private String email;
+	@NotBlank
 	private String name;
 	
 	
