1960f0912bc06618808ae46fc6bed72f10f5fd9f
alalal93 <alalal9310@gmail.com>
2024-02-26 19:22:07 +0900

    수정완

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -9,31 +9,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // 해당 파일로 시큐리티를 활성화
 @Configuration // Ioc
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨.
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/", "/user/**", "/image/**", "/subscibe/**", "/comment/**")
+				.authenticated().anyRequest().permitAll().and().formLogin().loginPage("/auth/signin") // GET
+				.loginProcessingUrl("/auth/signin") // POST -> 스프링 시큐리티가 로그인 프로세스 진행
+				.defaultSuccessUrl("/");
+
+	}
+
 	@Bean
 	public BCryptPasswordEncoder encode() {
 		return new BCryptPasswordEncoder();
-	}
-	
-	
-	
-	
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		//super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨.
-		http.csrf().disable();
-		http.authorizeRequests()
-			.antMatchers( "/", "/user/**", "/image/**", "/subscibe/**", "/comment/**").authenticated()
-			.anyRequest().permitAll()
-			.and()
-			.formLogin()
-			.loginPage("/auth/signin") // GET
-			.loginProcessingUrl("/auth/signin") // POST -> 스프링 시큐리티가 로그인 프로세스 진행
-			.defaultSuccessUrl("/");
-		
-		
 	}
 
 }
