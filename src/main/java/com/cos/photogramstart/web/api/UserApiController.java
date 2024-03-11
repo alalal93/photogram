9b0dd094eec9c6e26ac421072c6d802e5015f1b5
alalal93 <alalal9310@gmail.com>
2024-03-11 20:22:14 +0900

    aop공통처리

--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -60,25 +60,8 @@ public class UserApiController {
 		@Valid	UserUpdateDto userUpdateDto,
 		BindingResult bindingResult, // 꼭@Valid 가 적혀있는 다음 파라메터 적어야됨
 		@AuthenticationPrincipal PrincipalDetails principalDetails) {
-		
-		if (bindingResult.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-
-			for (FieldError error : bindingResult.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-				System.out.println("===============");
-				System.out.println(error.getDefaultMessage());
-				System.out.println("===============");
-			}
-			throw new CustomValidationApiException("유효성검사 실패",errorMap);
-		}else {
-			
-			User userEntity = userService.회원수정(id, userUpdateDto.toEntity());
+					User userEntity = userService.회원수정(id, userUpdateDto.toEntity());
 			principalDetails.setUser(userEntity); //세션 정보 변경
 			return new CMRespDto<>(1, "회원수정완료", userEntity);	// 응답시에 userEntity의 모든 getter 함수가 호출되고 JSON으로 파싱하여 응답
-		
-		}
-		
-		
 	}
 }
--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -179,7 +179,7 @@
 			alert("댓글을 작성해주세요!");
 			return;
 		}
-		
+
 		$.ajax({
 			type:"post",
 			url: "/api/comment",
